5c78107be521aadd8ee02dbd3221c28fabf61acb
Haridu Senadeera <Haridu@dhcp-10-105-90-24.wireless.northwestern.private>
2015-06-23 13:01:52 -0500

    changes to the nav component

---
+++ src/test.js
@@ -0,0 +1,12 @@
+var MyComponent = React.createClass({
+    render: function(){
+        return (
+            <a href="http://www.google.com">Haridu Senadeera</a>
+        );
+    }
+});
+
+React.render(
+    <MyComponent/>,
+    document.getElementById('myDiv')
+);
---
+++ src/test2.js
@@ -0,0 +1,13 @@
+var CheckLink = React.createClass({
+  render: function() {
+    // This takes any props passed to CheckLink and copies them to <a>
+    return <a {...this.props}>{'√ '}{this.props.children}</a>;
+  }
+});
+
+React.render(
+  <CheckLink href="/checked.html">
+    Click here!
+  </CheckLink>,
+  document.getElementById('content')
+);
