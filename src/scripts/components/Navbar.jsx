e7932781f546e59828f7911b2ae47f75545c3cc9
Haridu Senadeera <Haridu@dhcp-10-105-90-24.wireless.northwestern.private>
2015-07-16 14:43:26 -0500

    indentation changes

--- src/scripts/components/Navbar.jsx
+++ src/scripts/components/Navbar.jsx
@@ -1,28 +1,25 @@
-
 import React from 'react';
 import Radium from 'radium';
 
 @Radium
-export default class Navbar extends React.Component {
-  displayName = 'Navigation bar parent'
-
-  getStyles = () => {
-    return {
+export class Navbar extends React.Component {
+    displayName = 'Navigation bar parent'
+    getStyles = () => {
+      return {
             base: {
                 backgroundColor: '#f8f8f8',
                 borderColor: '#e7e7e7'
             }
-    };
-  }
-
-  render() {
-    const defStyle = this.getStyles();
-    return (
-      <nav style={[defStyle.base]} role="navigation">
-        <div className="container-fluid">
-          {this.props.children}
-        </div>
-      </nav>
-    );
-  }
+      };
+    }
+    render() {
+      const defStyle = this.getStyles();
+      return (
+						<nav style={[defStyle.base]} role="navigation">
+							<div className="container-fluid">
+								{this.props.children}
+							</div>
+						</nav>
+      );
+    }
 }
