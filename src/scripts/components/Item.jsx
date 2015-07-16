5448fc0484a86354f42aaa4b2865a2ee3162f91d
Haridu Senadeera <Haridu@dhcp-10-105-90-24.wireless.northwestern.private>
2015-07-16 16:59:24 -0500

    adding inline css

--- src/scripts/components/Item.jsx
+++ src/scripts/components/Item.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import Radium from 'radium';
 
-export default class Item extends React.Component {
-	
-	render() {
-		return (
-			<a href={this.props.link}>{this.props.title}</a>
-		)
-	}
+@Radium
+export class Item extends React.Component {
+    displayName = 'A fluid container'
+    getStyles = () => {
+      return {
+            base: {
+                paddingTop: '10px',
+                paddingBottom: '10px',
+                lineHeight: '20px',
+                color: '#777',
+
+                ':hover': {
+                    color: '#333',
+                    backgroundColor: 'transparent'
+                },
+
+                ':focus': {
+                    color: '#333',
+                    backgroundColor: 'transparent'
+                }
+            },
+            fixed: {
+                '@media (min-width: 768px)': {
+                    paddingTop: '15px',
+                    paddingBottom: '15px'
+                }
+            }
+        };
+    }
+    render() {
+      const defStyle = this.getStyles();
+      return (
+            <a style= {[defStyle.base, defStyle.fixed]} href={this.props.link}>
+                {this.props.title}
+            </a>
+        );
+    }
 }
