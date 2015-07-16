5082c43cd928694d6e5b75df6ee0e5d17ad72096
Haridu Senadeera <Haridu@dhcp-10-105-90-24.wireless.northwestern.private>
2015-07-16 16:50:11 -0500

    removed radium css

--- src/scripts/components/NavbarHeader.jsx
+++ src/scripts/components/NavbarHeader.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 export default class NavbarHeader extends React.Component {
-	
-	render() {
-		return (
-			<div className="navbar-header">
-				 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-				<a className="navbar-brand" href={this.props.href}>{this.props.name}</a>
-			</div>
-		)
-	}
+  displayName = 'Navbar Header'
+  render() {
+    return (
+      <div className="navbar-header">
+        <a className="navbar-brand" href={this.props.href}>{this.props.name}</a>
+      </div>
+    );
+  }
 }
