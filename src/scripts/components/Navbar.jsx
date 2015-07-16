cc2c5bd98d33eed7ea54324e48f9ba003d86c513
Haridu Senadeera <Haridu@dhcp-10-105-90-24.wireless.northwestern.private>
2015-07-16 16:28:00 -0500

    removed radium css

--- src/scripts/components/Navbar.jsx
+++ src/scripts/components/Navbar.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import Radium from 'radium';
 
-@Radium
-export class Navbar extends React.Component {
-	displayName = 'Navigation bar parent'
-	getStyles = () => {
-		return {
-			base: {
-				backgroundColor: '#f8f8f8',
-				borderColor: '#e7e7e7'
-			}
-		};
-	}
-	render() {
-		const defStyle = this.getStyles();
-		return (
-		<nav style={[defStyle.base]} role="navigation">
-			<div className="container-fluid">
-				{this.props.children}
-			</div>
-		</nav>
-	);
-	}
+export default class Navbar extends React.Component {
+  displayName = 'Navbar parent component'
+  render() {
+    return (
+      <nav className = "navbar navbar-default" role = "navigation">
+        <div className = "container-fluid">
+        {this.props.children}
+        </div>
+      </nav>
+    );
+  }
 }
