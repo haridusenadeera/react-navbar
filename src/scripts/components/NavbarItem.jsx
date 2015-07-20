d2a8e410822fe5aec56f9ea8033d72c371473e98
Haridu Senadeera <Haridu@Haridus-MacBook-Pro.local>
2015-07-20 17:08:41 -0500

    added inline css

--- src/scripts/components/NavbarItem.jsx
+++ src/scripts/components/NavbarItem.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import Radium from 'radium';
 
-
+@Radium
 export default class NavbarItem extends React.Component {
-	
-	render() {
-		return (
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    displayName = 'Navigation bar item list'
+    getStyles = () => {
+      return {
+            base: {
+                display: 'block',
+                margin: '7.5px -15px',
+                listStyle: 'none',
+                paddingLeft: '0',
+                boxSizing: 'border-box',
+
+                ':li': {
+                    cssFloat: 'left'
+                },
+
+                '@media (min-width: 768px)': {
+                    cssFloat: 'left',
+                    margin: '0'
+                }
+            },
+            list: {
+                cssFloat: 'left'
+            }
+        };
+    }
+    render() {
+      const defStyle = this.getStyles();
+      return (
                 <ul className="nav navbar-nav">
                             {
                                 this.props.children.map(child => {
-                                       return  <li>{child}</li>
-                                })
+                                return <li>{child}</li>; })
                             }
                 </ul>
-            </div>
-        )
-	}
+        );
+    }
 }
