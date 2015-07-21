49200e88327a099b2ae5acca9041707718ab4550
Haridu Senadeera <Haridu@dhcp-10-105-84-119.wireless.northwestern.private>
2015-07-21 15:52:30 -0500

    indentation changes

--- src/scripts/components/NavbarHeader.jsx
+++ src/scripts/components/NavbarHeader.jsx
@@ -8,36 +8,36 @@ export default class NavbarHeader extends React.Component {
       return {
             base: {
                 float: 'left',
-				marginRight: '0px',
-				marginLeft: '0px',
-				boxSizing: 'border-box'
+                marginRight: '0px',
+                marginLeft: '0px',
+                boxSizing: 'border-box'
             },
             brand: {
-				color: '#777',
-				float: 'left',
-				height: '50px',
-				padding: '15px 15px',
-				fontSize: '18px',
-				lineHeight: '20px',
-				textDecoration: 'none',
-				backgroundColor: 'transparent',
-				boxSizing: 'border-box',
-				fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
+                color: '#777',
+                float: 'left',
+                height: '50px',
+                padding: '15px 15px',
+                fontSize: '18px',
+                lineHeight: '20px',
+                textDecoration: 'none',
+                backgroundColor: 'transparent',
+                boxSizing: 'border-box',
+                fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
 
-				':hover': {
-					textDecoration: 'none',
-					color: '#5e5e5e',
-					backgroundColor: 'transparent'
+                ':hover': {
+                    textDecoration: 'none',
+                    color: '#5e5e5e',
+                    backgroundColor: 'transparent'
                 },
 
-				':focus': {
-					textDecoration: 'none',
-					color: '#5e5e5e',
-					backgroundColor: 'transparent'
+                ':focus': {
+                    textDecoration: 'none',
+                    color: '#5e5e5e',
+                    backgroundColor: 'transparent'
                 },
 
                 '@media (min-width: 768px)': {
-					marginLeft: '-15px'
+                    marginLeft: '-15px'
                 }
             }
         };
@@ -46,9 +46,9 @@ export default class NavbarHeader extends React.Component {
       const defStyle = this.getStyles();
       const {href, name} = this.props;
       return (
-		<div style= {[defStyle.base]}>
-			<a style= {[defStyle.brand]} href={href}>{name}</a>
-		</div>
+        <div style= {[defStyle.base]}>
+            <a style= {[defStyle.brand]} href={href}>{name}</a>
+        </div>
         );
     }
 }
