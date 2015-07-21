f2e4c9a6c7319660d96de6a487da0f3d48b3c439
Haridu Senadeera <Haridu@dhcp-10-105-84-119.wireless.northwestern.private>
2015-07-21 15:48:00 -0500

    inline css

--- src/scripts/components/Navbar.jsx
+++ src/scripts/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import Radium from 'radium';
 
+//@Radium
 export default class Navbar extends React.Component {
-  displayName = 'Navbar parent component'
-  render() {
-    return (
-      <nav className = "navbar navbar-default" role = "navigation">
-        <div className = "container-fluid">
-        {this.props.children}
-        </div>
-      </nav>
-    );
-  }
+    displayName = 'Navigation bar item link'
+    getStyles = () => {
+      return {
+            base: {
+                paddingRight: '15px',
+                paddingLeft: '15px',
+                marginRight: 'auto',
+                marginLeft: 'auto'
+            }
+        };
+    }
+    render() {
+      const defStyle = this.getStyles();
+      return (
+          <nav className = "navbar navbar-default" role = "navigation">
+            <div className = "container-fluid">
+            {this.props.children}
+            </div>
+          </nav>
+        );
+    }
 }
