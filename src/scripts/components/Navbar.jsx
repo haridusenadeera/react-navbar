8b9875dd4bb76d060c0a1f9c0fb02fab0e94bb45
Haridu Senadeera <Haridu@dhcp-10-105-84-119.wireless.northwestern.private>
2015-07-23 16:40:58 -0500

    added inline css

--- src/scripts/components/Navbar.jsx
+++ src/scripts/components/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import Radium from 'radium';
 
-//@Radium
+@Radium
 export default class Navbar extends React.Component {
-    displayName = 'Navigation bar item link'
+    displayName = 'Navigation bar'
     getStyles = () => {
       return {
-            base: {
-                paddingRight: '15px',
-                paddingLeft: '15px',
-                marginRight: 'auto',
-                marginLeft: 'auto'
-            }
-        };
+          navbar: {
+              backgroundColor: '#f8f8f8',
+              border: '1px solid transparent',
+              borderColor: '#e7e7e7',
+              borderRadius: '4px',
+              position: 'relative',
+              minHeight: '50px',
+              marginBottom: '20px',
+              display: 'block',
+              boxSizing: 'border-box',
+              zIndex: '1000',
+              borderWidth: '0 0 1px',
+
+              '@media (min-width: 768px)': {
+                  borderRadius: '0'
+              }
+          },
+          container: {
+              paddingRight: '15px',
+              paddingLeft: '15px',
+              marginRight: 'auto',
+              marginLeft: 'auto',
+              boxSizing: 'border-box'
+          }
+      };
     }
     render() {
       const defStyle = this.getStyles();
       return (
-          <nav className = "navbar navbar-default" role = "navigation">
-            <div className = "container-fluid">
-            {this.props.children}
+          <nav style= {[defStyle.navbar]}>
+            <div style= {[defStyle.container]}>
+              {this.props.children}
             </div>
           </nav>
         );
