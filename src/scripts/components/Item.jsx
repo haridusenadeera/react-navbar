33f2d9c3c6bcaf0dd9f5d4e3e86ed95816fb76b0
Haridu Senadeera <Haridu@dhcp-10-105-84-119.wireless.northwestern.private>
2015-07-23 15:47:43 -0500

    adding style proptype

--- src/scripts/components/Item.jsx
+++ src/scripts/components/Item.jsx
@@ -4,42 +4,48 @@ import Radium from 'radium';
 @Radium
 export default class Item extends React.Component {
     displayName = 'Navigation bar item link'
+
+    static propTypes = {
+        style: React.PropTypes.object
+    }
+
     getStyles = () => {
       return {
-            base: {
-                padding: '10px 15px',
-                lineHeight: '20px',
-                color: '#777',
-                position: 'relative',
-                display: 'block',
-                textDecoration: 'none',
-                backgroundColor: 'transparent',
-                boxSizing: 'border-box',
+          base: {
+              padding: '10px 15px',
+              lineHeight: '20px',
+              color: '#777',
+              position: 'relative',
+              display: 'block',
+              textDecoration: 'none',
+              backgroundColor: 'transparent',
+              boxSizing: 'border-box',
 
-                ':hover': {
-                    color: '#333',
-                    backgroundColor: 'transparent'
-                },
+              ':hover': {
+                  color: '#333',
+                  backgroundColor: 'transparent'
+              },
 
-                ':focus': {
-                    color: '#333',
-                    backgroundColor: 'transparent'
-                },
+              ':focus': {
+                  color: '#333',
+                  backgroundColor: 'transparent'
+              },
 
-                '@media (min-width: 768px)': {
-                    paddingTop: '15px',
-                    paddingBottom: '15px'
-                }
-            }
-        };
+              '@media (min-width: 768px)': {
+                  paddingTop: '15px',
+                  paddingBottom: '15px'
+              }
+          }
+      };
     }
+
     render() {
       const defStyle = this.getStyles();
-      const {link, title} = this.props;
+      const {link, title, style} = this.props;
       return (
-            <a style= {[defStyle.base]} href={link}>
-                {title}
-            </a>
+          <a style= {[defStyle.base, style && style]} href={link}>
+              {title}
+          </a>
         );
     }
 }
