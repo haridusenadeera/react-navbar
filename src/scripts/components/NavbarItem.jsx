9524266a5fec8f6340c03823d0239dc0aebd8d5c
Haridu Senadeera <Haridu@dhcp-10-105-84-119.wireless.northwestern.private>
2015-07-23 16:41:46 -0500

    changed children map function

--- src/scripts/components/NavbarItem.jsx
+++ src/scripts/components/NavbarItem.jsx
@@ -6,41 +6,49 @@ export default class NavbarItem extends React.Component {
     displayName = 'Navigation list of items'
     getStyles = () => {
       return {
-            base: {
-                margin: '7.5px -15px',
-                listStyle: 'outside none none',
-                paddingLeft: '0',
-                boxSizing: 'border-box',
+          base: {
+              float: 'left',
+              margin: '7.5px -15px',
+              listStyle: 'outside none none',
+              paddingLeft: '0',
+              boxSizing: 'border-box',
 
-                fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
-                fontSize: '14px',
-                color: '#333',
+              fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif',
+              fontSize: '14px',
+              color: '#333',
 
-                '@media (min-width: 768px)': {
-                    cssFloat: 'left',
-                    margin: '0'
-                }
-            },
-            list: {
-                float: 'left',
-                position: 'relative',
-                display: 'block',
-                boxSizing: 'border-box'
-            }
-        };
+              '@media (min-width: 768px)': {
+                  cssFloat: 'left',
+                  margin: '0'
+              }
+          },
+          list: {
+              float: 'left',
+              position: 'relative',
+              display: 'block',
+              boxSizing: 'border-box'
+          }
+      };
     }
+
+    renderChildren() {
+      const defStyle = this.getStyles();
+      const {listyle} = this.props;
+      return React.Children.map(this.props.children, child => {
+        return (
+          <li style= {[defStyle.list, listyle && listyle]}>
+            {child}
+          </li>);
+      });
+    }
+
     render() {
       const defStyle = this.getStyles();
+      const {style} = this.props;
       return (
-                <ul style= {[defStyle.base]}>
-                            {
-                                this.props.children.map(child => {
-                                  return (<li
-                                    key={this.props.children.indexOf(child)}
-                                    style= {[defStyle.list]}>{child}
-                                </li>); })
-                            }
-                </ul>
-        );
+        <ul style= {[defStyle.base, style && style]}>
+          {this.renderChildren()}
+        </ul>
+      );
     }
 }
