7e0b3ad0d05f82975c7dc5390721554f77ea1faa
Haridu <haridu.naveen@gmail.com>
2015-08-07 13:27:21 -0500

    added proptypes

--- src/scripts/components/NavbarDropdown.jsx
+++ src/scripts/components/NavbarDropdown.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 export default class NavbarDropdown extends React.Component {
     displayName = 'Navigation bar dropdown button'
 
+    static propTypes = {
+        name: React.PropTypes.string
+    }
+
     render() {
+      const {name, children} = this.props;
       return (
         <li className="dropdown">
             <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            Dropdown
+            {name}
             <span className="caret"></span>
             </a>
-            {this.props.children}
+            {children}
         </li>
       );
     }
