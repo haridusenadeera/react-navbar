08ca39a2e991e179f4e81ee7db580ea3c9c8c536
Haridu <haridu.naveen@gmail.com>
2015-08-17 16:51:48 -0500

    added navlink component

--- src/scripts/components/Item.jsx
+++ src/scripts/components/Item.jsx
@@ -1,50 +1,25 @@
 import React from 'react';
 import Radium from 'radium';
+import NavLink from './NavLink';
 
 @Radium
 export default class Item extends React.Component {
     displayName = 'Navigation bar item'
 
     static propTypes = {
-        link:    React.PropTypes.string,
-        title:   React.PropTypes.string,
-        liStyle: React.PropTypes.object,
-        aStyle:  React.PropTypes.object
+        link:  React.PropTypes.string,
+        title: React.PropTypes.string,
+        style: React.PropTypes.object
     }
 
     getStyles = () => {
       return {
-          list: {
+          base: {
               position: 'relative',
               display: 'block',
 
               '@media (min-width: 768px)': {
                   float: 'left'
-              }
-          },
-          link: {
-              padding: '10px 15px',
-              lineHeight: '20px',
-              position: 'relative',
-              display: 'block',
-              boxSizing: 'border-box',
-              textDecoration: 'none',
-              color: '#777',
-              backgroundColor: 'transparent',
-
-              ':hover': {
-                  color: '#333',
-                  backgroundColor: 'transparent'
-              },
-
-              ':focus': {
-                  color: '#333',
-                  backgroundColor: 'transparent'
-              },
-
-              '@media (min-width: 768px)': {
-                  paddingTop: '15px',
-                  paddingBottom: '15px'
               }
           }
       };
@@ -52,13 +27,11 @@ export default class Item extends React.Component {
 
     render() {
       const defStyle = this.getStyles();
-      const {aStyle, liStyle, link, title} = this.props;
+      const {style, link, title} = this.props;
       return (
-          <li key="list" style={[defStyle.list, liStyle && liStyle]}>
-              <a key ="link"style ={[defStyle.link, aStyle && aStyle]} href={link}>
-                  {title}
-              </a>
-          </li>
+        <li key="list" style={[defStyle.base, style && style]}>
+            <NavLink href={link} name={title} />
+        </li>
       );
     }
 }
