0c8a7ebcf3cf7c96cb23441a0656905eac0e8f08
Haridu <haridu.naveen@gmail.com>
2015-08-24 15:32:06 -0500

    adding navbar collapse css

--- src/scripts/components/NavbarItems.jsx
+++ src/scripts/components/NavbarItems.jsx
@@ -24,6 +24,33 @@ export default class NavbarItems extends React.Component {
                   float: 'left',
                   margin: '0'
               }
+          },
+          collapse: {
+              borderColor: '#E7E7E7',
+              marginRight: '-15px',
+              marginLeft: '-15px',
+              maxHeight: '340px',
+              paddingRight: '15px',
+              paddingLeft: '15px',
+              overflowX: 'visible',
+              borderTop: '1px solid transparent',
+              boxShadow: '0px 1px 0px rgba(255, 255, 255, 0.1)',
+              display: 'none',
+              boxSizing: 'border-box',
+
+              '@media (min-width: 768px)': {
+                  marginRight: '0px',
+                  marginLeft: '0px',
+                  paddingRight: '0px',
+                  paddingLeft: '0px',
+                  height: 'auto',
+                  paddingBottom: '0px',
+                  display: 'block',
+                  overflow: 'visible',
+                  width: 'auto',
+                  borderTop: '0px none',
+                  boxShadow: 'none'
+              }
           }
       };
     }
@@ -32,8 +59,8 @@ export default class NavbarItems extends React.Component {
       const defStyle = this.getStyles();
       const {style, children} = this.props;
       return (
-          <div>
-              <ul style={[defStyle.base, style && style]}>
+          <div ref ="collapse" style={[defStyle.collapse]}>
+              <ul ref= "navitems" style={[defStyle.base, style && style]}>
                   {children}
               </ul>
           </div>
