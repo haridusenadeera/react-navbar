a36d9ca8ce47190a21c32d4a8261a3d12744ffb8
Haridu <haridu.naveen@gmail.com>
2015-10-05 16:48:50 -0500

    initial commit

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-  entry: path.resolve(__dirname, 'src/app.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-};
