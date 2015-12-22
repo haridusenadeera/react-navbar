9f066ed15fc04392668373de37008ce0841947b2
Haridu <haridu.naveen@gmail.com>
2015-12-22 11:35:25 -0600

    added index file

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Navbar: require('./components/Navbar.jsx'),
+    NavItem: require('./components/NavItem.jsx'),
+    NavbarHeader: require('./components/NavbarHeader.jsx'),
+    NavbarItems: require('./components/NavbarItems.jsx'),
+    NavbarDropdown: require('./components/NavbarDropdown.jsx'),
+    DropdownMenu: require('./components/DropdownMenu.jsx')
+};
