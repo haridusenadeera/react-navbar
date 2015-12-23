e98f557c7efc03fb85907f502df86748e55599b6
Haridu <haridu.naveen@gmail.com>
2015-12-22 20:57:14 -0600

    resolve extention

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    Navbar: require('./components/Navbar.jsx'),
-    NavItem: require('./components/NavItem.jsx'),
-    NavbarHeader: require('./components/NavbarHeader.jsx'),
-    NavbarItems: require('./components/NavbarItems.jsx'),
-    NavbarDropdown: require('./components/NavbarDropdown.jsx'),
-    DropdownMenu: require('./components/DropdownMenu.jsx')
+    Navbar: require('./components/Navbar'),
+    NavItem: require('./components/NavItem'),
+    NavbarHeader: require('./components/NavbarHeader'),
+    NavbarItems: require('./components/NavbarItems'),
+    NavbarDropdown: require('./components/NavbarDropdown'),
+    DropdownMenu: require('./components/DropdownMenu')
 };
